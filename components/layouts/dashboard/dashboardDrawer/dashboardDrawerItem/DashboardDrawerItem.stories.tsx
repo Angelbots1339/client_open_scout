bf31557bae57e05b9943d829b4315f657d3d9d48
Dresden6 <dresdenfriar@icloud.com>
2022-10-27 15:36:59 -0600

    Fix import errors to get pull request through

--- components/layouts/dashboard/dashboardDrawer/dashboardDrawerItem/DashboardDrawerItem.stories.tsx
+++ components/layouts/dashboard/dashboardDrawer/dashboardDrawerItem/DashboardDrawerItem.stories.tsx
@@ -1,6 +1,8 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import DashboardDrawerItem, { IDashboardDrawerItem } from './DashboardDrawerItem';
-import { defaultDashboardDrawerItemProps } from './DashboardDrawerItem.mocks';
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta } from '@storybook/react';
+// import DashboardDrawerItem, { IDashboardDrawerItem } from './DashboardDrawerItem';
+import DashboardDrawerItem from './DashboardDrawerItem';
+// import { defaultDashboardDrawerItemProps } from './DashboardDrawerItem.mocks';
 
 export default {
     title: 'layouts/dashboard/dashboardDrawer/dashboardDrawerItem/DashboardDrawerItem',
@@ -10,13 +12,13 @@ export default {
 } as ComponentMeta<typeof DashboardDrawerItem>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof DashboardDrawerItem> = (args) => (
-    <DashboardDrawerItem {...args} />
-);
-
-export const SDashboardDrawerItem = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-
-SDashboardDrawerItem.args = {
-    ...defaultDashboardDrawerItemProps.base,
-} as IDashboardDrawerItem;
+// const Template: ComponentStory<typeof DashboardDrawerItem> = (args) => (
+//     <DashboardDrawerItem {...args} />
+// );
+//
+// export const SDashboardDrawerItem = Template.bind({});
+// // More on args: https://storybook.js.org/docs/react/writing-stories/args
+//
+// SDashboardDrawerItem.args = {
+//     ...defaultDashboardDrawerItemProps.base,
+// } as IDashboardDrawerItem;
