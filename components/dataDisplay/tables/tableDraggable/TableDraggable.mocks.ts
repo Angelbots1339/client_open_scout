4b04db344241cc5fdb10f61b1160b8261e762a8c
Dresden6 <dresdenfriar@icloud.com>
2022-11-08 14:45:36 -0700

    Fix TableDraggable mock props

--- components/dataDisplay/tables/tableDraggable/TableDraggable.mocks.ts
+++ components/dataDisplay/tables/tableDraggable/TableDraggable.mocks.ts
@@ -1,6 +1,43 @@
 import {ITableDraggable} from "./TableDraggable";
 
+const testData = [
+    {
+        name: "Ethan B",
+        assignedTeam: "1678",
+        timeScouted: "7",
+
+    },
+    {
+        name: "Dresden F",
+        assignedTeam: "7157",
+        timeScouted: "0",
+
+    },
+    {
+        name: "Nathan B",
+        assignedTeam: "1619",
+        timeScouted: "10",
+    },
+    {
+        name: "Joy D",
+        assignedTeam: "1410",
+        timeScouted: "3",
+
+    },
+    {
+        name: "Kellan C",
+        assignedTeam: "1690",
+        timeScouted: "10",
+
+    }
+];
+
 const base: ITableDraggable = {
+    data: testData,
+    nameKey: "name",
+    headerName: "Scouts",
+    keysToDisplay: "any",
+    displayNames: ["Assigned Team", "Time Scouted"],
 };
 
 export const defaultTableDraggableProps = {
